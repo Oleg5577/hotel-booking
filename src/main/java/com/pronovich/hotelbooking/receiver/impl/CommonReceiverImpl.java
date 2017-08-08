5c4d2f8ca55f9724f5a99c7541410894e52864a1
Oleg Pronovich <oleg.pronovich@gmail.com>
2017-08-08 23:42:50 +0300

    Initial commit

--- src/main/java/com/pronovich/hotelbooking/receiver/impl/CommonReceiverImpl.java
+++ src/main/java/com/pronovich/hotelbooking/receiver/impl/CommonReceiverImpl.java
@@ -15,6 +15,7 @@ import com.pronovich.hotelbooking.entity.RoomRequest;
 import com.pronovich.hotelbooking.entity.User;
 import com.pronovich.hotelbooking.exception.DaoException;
 import com.pronovich.hotelbooking.receiver.CommonReceiver;
+import com.pronovich.hotelbooking.validator.ContentValidator;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.validator.routines.EmailValidator;
 import org.apache.logging.log4j.LogManager;
@@ -30,66 +31,70 @@ public class CommonReceiverImpl implements CommonReceiver {
 
     private static final Logger LOGGER = LogManager.getLogger(CommonReceiverImpl.class);
 
-    private static final String EMAIL_PARAM = "email";
+//    private static final String EMAIL_PARAM = "email";
     private static final String PASSWORD_PARAM = "password";
-    private static final String REPEAT_PASSWORD_PARAM = "repeatPassword";
-    private static final String NAME_PARAM = "name";
-    private static final String SURNAME_PARAM = "surname";
-    private static final String PHONE_NUMBER_PARAM = "phoneNumber";
+//    private static final String REPEAT_PASSWORD_PARAM = "repeatPassword";
+//    private static final String NAME_PARAM = "name";
+//    private static final String SURNAME_PARAM = "surname";
+//    private static final String PHONE_NUMBER_PARAM = "phoneNumber";
 
     private static final int MIN_PASSWORD_SIZE = 6;
 
     @Override
     public void signUp(RequestContent content) {
-        String email = content.getRequestParameters().get(EMAIL_PARAM);
-        String password = content.getRequestParameters().get(PASSWORD_PARAM);
-        String repeatPassword = content.getRequestParameters().get(REPEAT_PASSWORD_PARAM);
-        String name = content.getRequestParameters().get(NAME_PARAM);
-        String surname = content.getRequestParameters().get(SURNAME_PARAM);
-        String phoneNumber = content.getRequestParameters().get(PHONE_NUMBER_PARAM);
+        //TODO validate in separate method
+        Map<String, String> wrongRequestValues = ContentValidator.signUpValidate(content);
 
-        //TODO add localization messages
-
-        Map<String, String> wrongRequestValues = new HashMap<>();
-
-        EmailValidator emailValidator = EmailValidator.getInstance();
-        if (StringUtils.isEmpty(email)) {
-            wrongRequestValues.put(EMAIL_PARAM, "Please enter a email");
-        } else if (!emailValidator.isValid(email)) {
-            wrongRequestValues.put(EMAIL_PARAM, "Email is not valid");
-        } else if (emailExists(email)) {
-            wrongRequestValues.put(EMAIL_PARAM, "Email already exists");
-        }
-
-        if (StringUtils.isEmpty(password)) {
-            wrongRequestValues.put(PASSWORD_PARAM, "Please, enter a Password");
-        } else if (password.length() < MIN_PASSWORD_SIZE) {
-            wrongRequestValues.put(PASSWORD_PARAM, "Enter " + MIN_PASSWORD_SIZE + " or more characters");
-        }
-
-        if (StringUtils.isEmpty(repeatPassword)) {
-            wrongRequestValues.put(REPEAT_PASSWORD_PARAM, "Please, repeat the Password");
-        } else if (!password.equals(repeatPassword)) {
-            wrongRequestValues.put(REPEAT_PASSWORD_PARAM, "Passwords don't match");
-        }
-
-        if (StringUtils.isEmpty(name)) {
-            wrongRequestValues.put(NAME_PARAM, "Please enter a Name");
-        }
-
-        if (StringUtils.isEmpty(surname)) {
-            wrongRequestValues.put(SURNAME_PARAM, "Please enter a Surname");
-        }
-
-        if (StringUtils.isEmpty(phoneNumber)) {
-            wrongRequestValues.put(PHONE_NUMBER_PARAM, "Please enter a Phone number");
-        }
+//        String email = content.getRequestParameters().get(EMAIL_PARAM);
+//        String password = content.getRequestParameters().get(PASSWORD_PARAM);
+//        String repeatPassword = content.getRequestParameters().get(REPEAT_PASSWORD_PARAM);
+//        String name = content.getRequestParameters().get(NAME_PARAM);
+//        String surname = content.getRequestParameters().get(SURNAME_PARAM);
+//        String phoneNumber = content.getRequestParameters().get(PHONE_NUMBER_PARAM);
+//
+//        //TODO add localization messages
+//
+//        Map<String, String> wrongRequestValues = new HashMap<>();
+//
+//        EmailValidator emailValidator = EmailValidator.getInstance();
+//        if (StringUtils.isEmpty(email)) {
+//            wrongRequestValues.put(EMAIL_PARAM, "Please enter a email");
+//        } else if (!emailValidator.isValid(email)) {
+//            wrongRequestValues.put(EMAIL_PARAM, "Email is not valid");
+//        } else if (emailExists(email)) {
+//            wrongRequestValues.put(EMAIL_PARAM, "Email already exists");
+//        }
+//
+//        if (StringUtils.isEmpty(password)) {
+//            wrongRequestValues.put(PASSWORD_PARAM, "Please, enter a Password");
+//        } else if (password.length() < MIN_PASSWORD_SIZE) {
+//            wrongRequestValues.put(PASSWORD_PARAM, "Enter " + MIN_PASSWORD_SIZE + " or more characters");
+//        }
+//
+//        if (StringUtils.isEmpty(repeatPassword)) {
+//            wrongRequestValues.put(REPEAT_PASSWORD_PARAM, "Please, repeat the Password");
+//        } else if (!password.equals(repeatPassword)) {
+//            wrongRequestValues.put(REPEAT_PASSWORD_PARAM, "Passwords don't match");
+//        }
+//
+//        if (StringUtils.isEmpty(name)) {
+//            wrongRequestValues.put(NAME_PARAM, "Please enter a Name");
+//        }
+//
+//        if (StringUtils.isEmpty(surname)) {
+//            wrongRequestValues.put(SURNAME_PARAM, "Please enter a Surname");
+//        }
+//
+//        if (StringUtils.isEmpty(phoneNumber)) {
+//            wrongRequestValues.put(PHONE_NUMBER_PARAM, "Please enter a Phone number");
+//        }
 
         if (!wrongRequestValues.isEmpty()) {
             content.addWrongValues(wrongRequestValues);
         } else {
             try {
                 byte[] salt = getSalt();
+                String password = content.getRequestParameters().get(PASSWORD_PARAM);
                 String securePassword = getSecurePassword(password, salt);
 
                 String encodedSalt = Base64.getEncoder().encodeToString(salt);
@@ -129,16 +134,16 @@ public class CommonReceiverImpl implements CommonReceiver {
         return salt;
     }
 
-    private boolean emailExists(String email) {
-        UserDao userDao = new UserDaoImpl();
-        boolean emailExists = false;
-        try {
-            emailExists = userDao.findUserByEmail(email) != null;
-        } catch (DaoException e) {
-            LOGGER.error("Check if email exists error", e);
-        }
-        return emailExists;
-    }
+//    private boolean emailExists(String email) {
+//        UserDao userDao = new UserDaoImpl();
+//        boolean emailExists = false;
+//        try {
+//            emailExists = userDao.findUserByEmail(email) != null;
+//        } catch (DaoException e) {
+//            LOGGER.error("Check if email exists error", e);
+//        }
+//        return emailExists;
+//    }
 
 
     @Override
